c68e114e28922f9b490262386a023afd9515d52c
Lewis J. Hannah <lewis.j.hannah@gmail.com>
2024-02-08 17:23:27 -0500

    Day 9 Completed

---
+++ ClientApp/src/app/register-passenger/register-passenger.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { PassengerService } from '../api/services';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-register-passenger',
+  templateUrl: './register-passenger.component.html',
+  styleUrls: ['./register-passenger.component.css']
+})
+export class RegisterPassengerComponent {
+  constructor(private passengerService: PassengerService,
+    private fb: FormBuilder) {}
+
+  form = this.fb.group({
+    email: [''],
+    firstName: [''],
+    lastName: [''],
+    isFemale: [true],
+  });
+
+  register() {
+    console.log("Form Values: ", this.form.value);
+    this.passengerService.registerPassenger({body: this.form.value})
+      .subscribe(_ => console.log("Form posted to Server"));
+  }
+}
---
+++ Dtos/NewPassengerDto.cs
@@ -0,0 +1,10 @@
+namespace Flights.Dtos
+{
+    public class NewPassengerDto {
+
+    public string Email { get; set; }
+    public string FirstName { get; set ;}
+    public string LastName { get; set; }
+    public bool Gender { get; set; }
+    }
+}
